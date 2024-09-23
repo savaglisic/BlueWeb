5c9b192fb496b4a08bddd1231b881d899006ceb8
Sava Glisic <savaglisic9@gmail.com>
2024-09-23 17:40:41 -0400

    global vars

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import MainMenu from './MainMenu';
 import SearchPedigreeDatabase from './SearchPedigreeDatabase';
 import AddSamples from './AddSamples'; 
 import ConfigureApp from './ConfigureApp';
+import { GlobalStyles, CssVarsProvider } from '@mui/joy';
 
 const App = () => {
   const [view, setView] = useState(localStorage.getItem('userEmail') ? 'mainMenu' : 'login');
@@ -14,6 +15,33 @@ const App = () => {
   };
 
   return (
+    <CssVarsProvider>
+    <GlobalStyles
+      styles={{
+        '::-webkit-scrollbar': {
+          width: '8px',
+        },
+        '::-webkit-scrollbar-track': {
+          background: 'transparent',
+        },
+        '::-webkit-scrollbar-thumb': {
+          backgroundColor: 'rgba(0, 0, 0, 0.3)',
+          borderRadius: '10px',
+          border: '2px solid transparent',
+          backgroundClip: 'padding-box',
+        },
+        '::-webkit-scrollbar-thumb:hover': {
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        },
+        '::-webkit-scrollbar-thumb:active': {
+          backgroundColor: 'rgba(0, 0, 0, 0.7)',
+        },
+        '*': {
+          scrollbarWidth: 'thin', // For Firefox
+          scrollbarColor: 'rgba(0, 0, 0, 0.3) transparent',
+        },
+      }}
+    />
     <>
       {view === 'login' && <LoginPage onLoginSuccess={handleLoginSuccess} />}
       {view === 'mainMenu' && <MainMenu setView={setView} />}
@@ -21,6 +49,7 @@ const App = () => {
       {view === 'addSamples' && <AddSamples setView={setView} />} 
       {view === 'configureApp' && <ConfigureApp setView={setView} />} 
     </>
+    </CssVarsProvider>
   );
 };
 
